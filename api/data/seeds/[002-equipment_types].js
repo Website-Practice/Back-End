031063c0a9ddc1937a75e88dc6fb0acda6d310ac
bukit3point0 <bukit3point0@gmail.com>
2021-06-28 16:44:51 -0400

    seed files empty but created

---
+++ api/data/seeds/[002-equipment_types].js
@@ -0,0 +1,8 @@
+
+exports.seed = function(knex) {
+  return knex('equipment_types').insert([
+    {id: 1, colName: 'rowValue1'},
+    {id: 2, colName: 'rowValue2'},
+    {id: 3, colName: 'rowValue3'}
+  ]);
+}
