f282272032c2f9fb0bd45c6592b8ef0187dc692c
natepace <n8wpace@gmail.com>
2021-07-16 14:46:19 -0600

    fixed seed

--- api/data/seeds/[003-equipment].js
+++ api/data/seeds/[003-equipment].js
@@ -5,21 +5,21 @@ exports.seed = function (knex) {
       equipment_name: "Shitty Ass Mac",
       equipment_description: "It doesn't work and I hate it so take it the fuck away",
       cost: 20.00,
-      owner: 1,
+      owner_id: 1,
       equipment_type_id: 3
     },
     {
       equipment_name: "Rat-chewed cables",
       equipment_description: "You heard me",
       cost: 5.00,
-      owner: 2,
+      owner_id: 2,
       equipment_type_id: 1
     },
     {
       equipment_name: "Broken phone",
       equipment_description: "Can't even take pictures. Give to your shit-ass 5 year old to make them feel cool",
       cost: 10.00,
-      owner: 3,
+      owner_id: 3,
       equipment_type_id: 2
     }
   ]);
